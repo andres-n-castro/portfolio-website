f230832d14ce5ab0e6a3670249313d74248e119d
Andres Castro <andresncastro12@gmail.com>
2026-07-19 13:17:31 -0400

    more changes to the navbar and downloaded react-icons for github and linkedin icons

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
+import Link from "next/link"
+import { CiLinkedin } from "react-icons/ci";
+import { VscGithub } from "react-icons/vsc";
 
 export default function NavBar() {
     return (
-        <div className="absolute flex border-4 border-white min-w-screen min-h-20 justify-end gap-4 px-8">
-            <button>
+        <div className="absolute flex border-4 items-start border-white min-w-screen min-h-20 justify-end gap-4 px-8">
+            <Link className="flex bg-red-700 rounded" href="/projects">   
                 ABOUT
-            </button> 
+            </Link> 
 
             <button>
                 PROJECTS
@@ -16,16 +18,11 @@ export default function NavBar() {
             </button>
 
             <button>
-                <Image
-                    src="/icons/Github_Icon.png"
-                    alt="Github"
-                    width={50}
-                    height={50}
-                />
+                <VscGithub className="text-white text-[30px]"/>
             </button>
 
             <button>
-                button 5
+                <CiLinkedin className="text-white text-[38px]"/>
             </button>   
 
         </div>
